d9cc6534ac6197ddd0d0b3ddf79b4f4a4a5489bb
Mohamed Khalfella <khalfella@gmail.com>
2018-04-17 11:39:00 +0000

    XDMA WIP 7

--- hw/xdma.c
+++ hw/xdma.c
@@ -17,7 +17,8 @@
 #define XDMA_COMM_UINT32(d, addr) (*((uint32_t *) &(d)->xdma_command[(addr)]))
 
 
-#define ROUND_UP(N, S) ((((N) + (S) - 1) / (S)) * (S))
+#define	ROUND_UP(N, S)		((((N) + (S) - 1) / (S)) * (S))
+#define	ROUND_DOWN(N,S)		(((N) / (S)) * (S))
 
 static xdma_ent_t *
 xdma_find_map(AssignedDevRegion *d, uint64_t gxoff, uint64_t hxphys)
@@ -26,13 +27,16 @@ xdma_find_map(AssignedDevRegion *d, uint64_t gxoff, uint64_t hxphys)
 	for (xde = list_head(&d->xdma_list); xde != NULL;
 	    xde = list_next(&d->xdma_list, xde)) {
 
+		if (xde->xd_flags == XDMA_ENT_FLAGS_SHADOW)
+			continue;
+
 		if (gxoff != 0 && gxoff >= xde->xd_gx_off &&
 		    gxoff < xde->xd_gx_off + xde->xd_length) {
 			return (xde);
 		}
 
 		if (hxphys != 0 && hxphys >= xde->xd_hx_phys &&
-		    gxoff < xde->xd_hx_phys + xde->xd_length) {
+		    hxphys < xde->xd_hx_phys + xde->xd_length) {
 			return (xde);
 		}
 	}
@@ -98,10 +102,80 @@ out:
 	return (1);
 }
 
+static void
+xdma_try_removal(AssignedDevRegion *d)
+{
+	xdma_ent_t *xde, *pxde;
+
+	for (xde = list_tail(&d->xdma_list); xde != NULL; xde = pxde) {
+
+		if (xde->xd_flags != XDMA_ENT_FLAGS_SHADOW)
+			break;
+
+		fprintf(stderr, "%s: removing xd_flags = %llx xd_type = %llx "
+		    "xd_length = %llx xd_gx_off = %llx xd_hx_phys = %llx "
+		    "xd_gb_vir = %llx xd_gb_phys = %llx xd_gb_off = %llx\n",
+		    __func__, xde->xd_flags, xde->xd_type,
+		    xde->xd_length, xde->xd_gx_off, xde->xd_hx_phys,
+		    xde->xd_gb_vir, xde->xd_gb_phys, xde->xd_gb_off);
+
+		fprintf(stderr, "%s: xdma_cur_offset before = %llx\n",
+		    __func__, d->xdma_cur_offset);
+
+		d->xdma_cur_offset -= xde->xd_length;
+		d->xdma_cur_offset = ROUND_DOWN(d->xdma_cur_offset, 4096);
+
+		fprintf(stderr, "%s: xdma_cur_offset after = %llx\n",
+		    __func__, d->xdma_cur_offset);
+
+		pxde = list_prev(&d->xdma_list, xde);
+		list_remove(&d->xdma_list, xde);
+		qemu_free(xde);
+	}
+}
+
 static uint32_t
 xdma_remove_map(AssignedDevRegion *d, xdma_cmd_t *xc)
 {
+	upci_dma_t ud;
+	xdma_ent_t *xde;
+
+	fprintf(stderr, "%s: g_xdma_off = %llx type = %llx "
+	    "dir = %llx length = %llx g_xdma_off = %llx h_xdma_phys = %llx\n"
+	    "gb_vir = %llx gb_phys = %llx gb_off = %llx\n",
+	    __func__, d->xdma_cur_offset, xc->xc_type,
+	    xc->xc_dir, xc->xc_size, xc->xc_gx_off, xc->xc_hx_phys,
+	    xc->xc_gb_vir, xc->xc_gb_phys, xc->xc_gb_off);
+
+	if ((xde = xdma_find_map(d, xc->xc_gx_off, xc->xc_hx_phys)) == NULL) {
+		goto error;
+	}
+
+	fprintf(stderr, "%s: found xd_flags = %llx xd_type = %llx "
+	    "xd_length = %llx xd_gx_off = %llx xd_hx_phys = %llx "
+	    "xd_gb_vir = %llx xd_gb_phys = %llx xd_gb_off = %llx\n",
+	    __func__, xde->xd_flags, xde->xd_type,
+	    xde->xd_length, xde->xd_gx_off, xde->xd_hx_phys,
+	    xde->xd_gb_vir, xde->xd_gb_phys, xde->xd_gb_off);
+
+	ud.ud_type = (xc->xc_type == XDMA_CMD_MAP_TYPE_COH) ?
+	    DDI_DMA_CONSISTENT : DDI_DMA_STREAMING;
+	ud.ud_write = 0;
+	ud.ud_length = 0;
+	ud.ud_rwoff = 0;
+	ud.ud_host_phys = xde->xd_hx_phys;
+	ud.ud_udata = 0;
+
+	if (ioctl(d->region->upci_fd, UPCI_IOCTL_XDMA_REMOVE, &ud) == 0) {
+		xde->xd_flags = XDMA_ENT_FLAGS_SHADOW;
+		fprintf(stderr, "%s: successfully removed from upci\n", __func__);
+		xdma_try_removal(d);
+		xc->xc_status = XDMA_CMD_STATUS_OK;
+		return (0);
+	}
+error:
 	fprintf(stderr, "%s: failed\n", __func__);
+	xc->xc_status = XDMA_CMD_STATUS_ER;
 	return (1);
 }
 
@@ -159,7 +233,7 @@ xdma_slow_bar_rw_common(AssignedDevRegion *d,
 	ud.ud_type = 0;
 	ud.ud_write = write;
 	ud.ud_length = len;
-	ud.ud_rwoff = addr - xde->xd_gx_off;
+	ud.ud_rwoff = (uintptr_t) addr - xde->xd_gx_off;
 	ud.ud_host_phys = xde->xd_hx_phys;
 	ud.ud_udata = val;
 
